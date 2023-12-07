9153b209e1c9c4f0928ed62c862d3fa45ac37489
tinymonster123 <lifenghan123@outlook.com>
2023-12-08 00:08:22 +0800

    feat:完成注册页面

--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -1,15 +1,24 @@
 import { useState, useRef } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Checkbox } from 'antd'
 
 
 export default function Register() {
     const [username, SetName] = useState("")
     const [password, SetPassword] = useState("")
     const [re_password, SetReName] = useState("")
+    const navigate = useNavigate()
     const checkboxRef = useRef(null)
+    const onChange = (e) => {
+        console.log(`checked = ${e.target.checked}`);
+    }
 
 
     const RegisterInformation = async () => {
 
+        const onChange = (e) => {
+            console.log(`checked = ${e.target.checked}`);
+        }
 
         if (!username || !password || !re_password) {
             alert("还有信息没有填哦")
@@ -20,27 +29,29 @@ export default function Register() {
             alert("您还没有勾选协议，请先勾选再注册")
         }
 
+
         try {
-            const response = await fetch('http://127.0.0.1:8080/signup', {
-                method: 'POST',
-                body: JSON.stringify({
-                    username,
-                    password,
-                    re_password,
-                }),
-                headers: { 'Content-Type': 'application/json' }
+            const response = await axios.post('http://127.0.0.1:8080/signup', {
+                username: username,
+                password: password,
+                re_password: re_password,
             })
 
+            const data = response.data
 
-            const data = response.json()
-            if (response.ok) {
+            localStorage.setItem('token', data.token)
 
-                localStorage.setItem('token', data.token)
-                alert('注册成功')
+            if (response.status === 200) {
+                alert("注册成功")
+
+                navigate('/Login')
+
             } else {
                 alert(`注册失败：${data.msg}`)
             }
-        } catch (error) {
+        }
+
+        catch (error) {
             alert(`请求注册出现错误：${error}`)
         }
 
@@ -52,26 +63,22 @@ export default function Register() {
 
         <>
             <div className="Allof">
-                {/* <img />等设计写好了登录页面再把src传上去 */}
                 <div className="Login-Box">
                     <div className='UserInformation'>
-                        <p className='word'>请输入您的姓名</p>
-                        <input type="text" value={username} placeholder='姓名' onChange={(e) => { SetName(e.target.value) }} className='TEXT' />
+                        <input type="text" value={username} placeholder='请输入用户名' onChange={(e) => { SetName(e.target.value) }} className='TEXT' />
                     </div>
                     <div className='UserInformation'>
-                        <p className='word'>请设置您的密码</p>
-                        <input type="password" value={password} placeholder='密码' onChange={(e) => { SetPassword(e.target.value) }} className='TEXT' />
+                        <input type="password" value={password} placeholder='请设置密码' onChange={(e) => { SetPassword(e.target.value) }} className='TEXT' />
                     </div>
                     <div className='UserInformation'>
-                        <p className='word'>请确认您的密码</p>
-                        <input type="password" value={re_password} placeholder='密码' onChange={(e) => { SetReName(e.target.value) }} className='TEXT' />
+                        <input type="password" value={re_password} placeholder='请确认密码' onChange={(e) => { SetReName(e.target.value) }} className='TEXT' />
                     </div>
                     <div className='LoginBtnBox'>
                         <button className='LoginBtn' onClick={RegisterInformation}>注册</button>
                     </div>
                 </div>
                 <div className='underFoot'>
-                    <input type='checkbox' ref={checkboxRef} />
+                    <Checkbox onChange={onChange} ref={checkboxRef}></Checkbox>
                     <span>登录/注册表示同意《今天吃饱了?团队出品协议》</span>
                 </div>
 

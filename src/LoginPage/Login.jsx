b7bb3ea24de302afb41fdb1e2bc4ce324cdbf7ba
tinymonster123 <lifenghan123@outlook.com>
2023-12-07 18:57:42 +0800

    fixed:修改请求头

--- src/LoginPage/Login.jsx
+++ src/LoginPage/Login.jsx
@@ -3,25 +3,21 @@ import './Login.css'
 import { create } from 'zustand'
 import { useNavigate } from 'react-router-dom'
 
-
+const SetUserState = create(set => ({
+    ifLogined: false,
+    setLogin: (state) => set({ ifLogined: state }),
+}))
 
 export default function Login() {
     const [username, SetName] = useState("")
     const [password, SetPassword] = useState('')
     const checkboxRef = useRef(null)
     const navigate = useNavigate()
+    const setLogin = SetUserState(state => state.setLogin)
 
     const Navigation = () => {
         navigate('/Register')
     }
-
-
-    const SetUserState = create(set => ({
-        ifLogined: false,
-        setLogin: (state) => set({ ifLogined: state }),
-    }))
-
-    const setLogin = SetUserState(state => state.setLogin)
 
     const PostUserInformation = async () => {
         if (!username || !password) {
@@ -35,20 +31,20 @@ export default function Login() {
         }
 
         try {
-            const response = await fetch('http://127.0.0.1:8080/login', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({
-                    username,
-                    password,
-                })
+            const response = await axios.post('http://127.0.0.1:8080/signup', {
+                username : username,
+                password : password ,
+            },{
+                headers : {
+                    "Authorization" : token ,
+                }
             })
 
-            const data = await response.json()
+            const data = response.data
 
-            if (response.ok) {
+            localStorage.setItem('token',data.token)
 
-                localStorage.setItem('token', data.token)
+            if (response.status === 200) {
                 alert("登录成功")
                 setLogin(true)
                 alert(`用户的登录状态为${SetUserState.getState().ifLogined}`)
@@ -67,6 +63,8 @@ export default function Login() {
     }
 
 
+
+
     return (
         <>
             <div className="Allof">
@@ -77,7 +75,6 @@ export default function Login() {
                         <p className='word'>请输入您的姓名</p>
                         <input type="text" value={username} placeholder='姓名' onChange={(e) => { SetName(e.target.value) }} className='TEXT' />
                     </div>
-                    <p className='word'>请输入您的密码</p>
                     <div className='UserInformation'>
                         <p className='word'>请输入您的密码</p>
                         <input type="password" value={password} placeholder='密码' onChange={(e) => { SetPassword(e.target.value) }} className='TEXT' />

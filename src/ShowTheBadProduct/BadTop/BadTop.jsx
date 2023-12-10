953a7ffe55bc750f4561a6f2c03d9804d68b158f
tinymonster123 <lifenghan123@outlook.com>
2023-12-10 15:11:57 +0800

    fixed：修改css文件的位置

--- src/ShowTheBadProduct/BadTop/BadTop.jsx
+++ src/ShowTheBadProduct/BadTop/BadTop.jsx
@@ -1,6 +1,6 @@
-import './BadTOP.css'
-import { useNavigate } from 'react-router-dom'
 
+import { useNavigate } from 'react-router-dom'
+import './BadTOP.css'
 
 export default function Top() {
     const navigate = useNavigate()
